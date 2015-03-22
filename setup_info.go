fd0bad3a5a3ded3a7491c4fd98d96e4fa0b8432f
oneplus1000 <oneplus1000@gmail.com>
2015-03-22 11:57:03 +0700

    fix HotReloadView value

--- setup_info.go
+++ setup_info.go
@@ -1,6 +1,7 @@
 package webinit
 
 type SetupInfo struct {
-	RootFolder                                 string //RootFolder, root folder that hold controllers,views and public
-	Addr, HotReloadView, DelimLeft, DelimRight string
+	RootFolder                  string //RootFolder, root folder that hold controllers,views and public
+	Addr, DelimLeft, DelimRight string
+	HotReloadView               bool
 }
