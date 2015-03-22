4109c106410c39ee813831980bb745ce8e9caa3d
oneplus1000 <oneplus1000@gmail.com>
2015-03-22 12:02:38 +0700

    comment on setup_info.go

--- setup_info.go
+++ setup_info.go
@@ -1,7 +1,11 @@
 package webinit
 
 type SetupInfo struct {
-	RootFolder                  string //RootFolder, root folder that hold controllers,views and public
-	Addr, DelimLeft, DelimRight string
-	HotReloadView               bool
+	RootFolder            string //RootFolder, root folder that hold controllers,views and public
+	Addr                  string //bind address  example  ":8080"
+	DelimLeft, DelimRight string
+
+	//true - load template where reload page (recommand when develop)
+	//false - load template only one time when  app stating (recommand when deploy)
+	HotReloadView bool
 }
